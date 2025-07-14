99a2027183b44a5445e20eca4a6b8e1be917a97a
new <122328306+Pedro-a17@users.noreply.github.com>
2025-07-14 19:44:25 -0300

    Create type.py
    types in python

---
+++ type.py
@@ -0,0 +1,12 @@
+num = 2
+str = "abc"
+float = 0.234
+bool = False
+complex = 1 + 2j
+print(type(num))
+print(type(str))
+print(type(float))
+print(type(bool))
+print(type(complex), isinstance(complex, int))
+
+  
