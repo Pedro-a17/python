4aa068af98a519c2b7ee052b446f914829a5065b
new <122328306+Pedro-a17@users.noreply.github.com>
2025-07-14 19:34:47 -0300

    Delete while.py

--- while.py
+++
@@ -1,9 +0,0 @@
-num = 0
-m = []
-while True:
-    num = input("digite um número um \"x\" para parar:")
-    if (num == "x" or num == "X"):
-        break
-    m.append(int(num))
-    print(m)
-print("fim")
