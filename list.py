241d2beecc79f1eb40730ab9dcb2229a97bb9522
new <122328306+Pedro-a17@users.noreply.github.com>
2025-07-14 21:09:57 -0300

    Create list.py

---
+++ list.py
@@ -0,0 +1,15 @@
+l1 = [1, 2 ,3]
+l2 = [4, 5 ,6]
+l1[-1], l1[0] = 4, 0
+l = l1 + l2
+print(l)
+print(l[0:2], l[3:4], l[-4: ])
+l.append(6)
+l.pop(1)
+print(l)
+l.insert(2, 2)
+print(l, len(l))
+l_sort = sorted(l, reverse=True)
+print(l_sort)
+print(max(l), min(l), sum(l))
+print(7 in l)
